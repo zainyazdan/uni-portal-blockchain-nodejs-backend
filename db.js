ac42f6ed531fd3dbac5cb00a4f2680d050fbbf52
zainyazdan <zainyazdan54321@gmail.com>
2020-05-08 01:35:01 +0500

    Procfile included

--- db.js
+++ db.js
@@ -1,15 +1,15 @@
 var mysql = require('mysql');
 
 
-/*
-var connection = mysql.createConnection({
-	//properties
-	host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'portaldb'
-});
-*/
+
+// var connection = mysql.createConnection({
+// 	//properties
+// 	host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'portaldb'
+// });
+
 
 
 var connection = mysql.createConnection({
