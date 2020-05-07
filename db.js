c8b9ade3ae4e09cbd87a7470a8d588f0eff4e791
zainyazdan <zainyazdan54321@gmail.com>
2020-05-08 04:15:09 +0500

    New Heroku Database Attached

--- db.js
+++ db.js
@@ -2,6 +2,8 @@ var mysql = require('mysql');
 
 
 
+
+
 // var connection = mysql.createConnection({
 // 	//properties
 // 	host: 'localhost',
@@ -12,16 +14,25 @@ var mysql = require('mysql');
 
 
 
+
 var connection = mysql.createConnection({
 	//properties
-	host: 'us-cdbr-east-06.cleardb.net',
-    user: 'b77bf3d69101eb',
-    password: 'ce0375b8',
-    database: 'heroku_bce77c5f82e9275'
+	host: 'u3r5w4ayhxzdrw87.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'n4pavnb3gzarryri',
+    password: 'cykg7qt9izbu50ob',
+    database: 'gvqo6gf79qtvgv62'
 });
 
 
+// var connection = mysql.createConnection({
+// 	host     : 'us-cdbr-east-06.cleardb.net',
+// 	user     : 'b77bf3d69101eb',
+// 	password : 'ce0375b8',
+// 	database : 'heroku_bce77c5f82e9275',
+// 	ConnectionLimit: 10
+//   });
 
+ 
 
 connection.connect(function(error){
 	if(error){
